71edf32003d52a83963ba06e41ab82f67a870132
Artyom <3Westers07P@gmail.com>
2022-02-07 23:21:27 +0300

    added almost all functions with eng website

--- scraper.py
+++ scraper.py
@@ -10,41 +10,40 @@ all_text = ""
 PATH = "C:\Program Files (x86)\chromedriver.exe"
 driver = webdriver.Chrome(PATH)
 
-driver.get("https://cointelegraph.com/search?query=btc")
+driver.get("https://currency.com/search")
 print(driver.title)
 
-search = driver.find_element(By.ID, "search")
-search.send_keys("eth")
+search = driver.find_element(By.XPATH, "/html/body/div[1]/form[1]/div/label/input")
+search.send_keys("etherium")
 search.send_keys(Keys.RETURN)
 
 
 container = WebDriverWait(driver, 10).until(
-    EC.presence_of_element_located((By.CLASS_NAME, "post_container"))
+    EC.presence_of_element_located((By.CLASS_NAME, "global-search__list"))
 )
 
-results = container.find_elements(By.CLASS_NAME, "row.result")
+results = container.find_elements(By.CLASS_NAME, "global-search__item")
 
 try:
     for i in range(1):
 
-
         container_el = WebDriverWait(driver, 10).until(
-            EC.presence_of_element_located((By.CLASS_NAME, "post_container"))
+            EC.presence_of_element_located((By.CLASS_NAME, "global-search__list"))
         )
 
-        results_el = container_el.find_elements(By.CLASS_NAME, "row.result")
+        results_el = container_el.find_elements(By.CLASS_NAME, "global-search__item")
     
-        a_container = results_el[i].find_element(By.CLASS_NAME, "header")
-        a = a_container.find_element(By.TAG_NAME, "a")
+        a = results_el[i].find_element(By.TAG_NAME, "a")
         a.click()
     
 
         section = WebDriverWait(driver, 5).until(
-            EC.presence_of_element_located((By.CLASS_NAME, "post__article"))
+            EC.presence_of_element_located((By.CLASS_NAME, "inner-content-article"))
         )
         print(section.text)
 
         driver.back()
+        driver.refresh()
 
 finally:
     driver.close()
