6f795f3cf2cb7e645d5a4e1e2afc91660c694179
Artyom <3Westers07P@gmail.com>
2022-02-09 23:34:56 +0300

    added search function

--- scraper.py
+++ scraper.py
@@ -4,54 +4,63 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import time
+import eel
 
+eel.init("web")
 
-all_text = ""
 PATH = "C:\Program Files (x86)\chromedriver.exe"
-driver = webdriver.Chrome(PATH)
 
-driver.get("https://currency.com/search")
-print(driver.title)
+@eel.expose
+def scraper(query):
 
-search = driver.find_element(By.XPATH, "/html/body/div[1]/form[1]/div/label/input")
-search.send_keys("etherium")
-search.send_keys(Keys.RETURN)
+    all_text = ""
+    driver = webdriver.Chrome(PATH)
+    driver.get("https://currency.com/search")
+    print(driver.title)
+
+    search = driver.find_element(By.XPATH, "/html/body/div[1]/form[1]/div/label/input")
+    search.send_keys(query)
+    search.send_keys(Keys.RETURN)
 
 
-container = WebDriverWait(driver, 10).until(
-    EC.presence_of_element_located((By.CLASS_NAME, "global-search__list"))
-)
+    container = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.CLASS_NAME, "global-search__list"))
+    )
 
-results = container.find_elements(By.CLASS_NAME, "global-search__item")
+    results = container.find_elements(By.CLASS_NAME, "global-search__item")
 
-try:
-    for i in range(1):
+    try:
+        for i in range(1):
 
-        container_el = WebDriverWait(driver, 10).until(
-            EC.presence_of_element_located((By.CLASS_NAME, "global-search__list"))
-        )
+            container_el = WebDriverWait(driver, 10).until(
+                EC.presence_of_element_located((By.CLASS_NAME, "global-search__list"))
+            )
 
-        results_el = container_el.find_elements(By.CLASS_NAME, "global-search__item")
+            results_el = container_el.find_elements(By.CLASS_NAME, "global-search__item")
 
-        img = results_el[i].find_element(By.TAG_NAME, "img")
-        src = img.get_attribute("src")
+            img = results_el[i].find_element(By.TAG_NAME, "img")
+            src = img.get_attribute("src")
 
-        a = results_el[i].find_element(By.TAG_NAME, "a")
-        a.click()
+            a = results_el[i].find_element(By.TAG_NAME, "a")
+            a.click()
     
         
-        section = WebDriverWait(driver, 5).until(
-            EC.presence_of_element_located((By.CLASS_NAME, "inner-content-article"))
-        )
-        all_text += section.text
+            section = WebDriverWait(driver, 5).until(
+                EC.presence_of_element_located((By.CLASS_NAME, "inner-content-article"))
+            )
+            all_text += section.text
+            title = driver.title
+            driver.back()
+            driver.refresh()
 
-        driver.back()
-        driver.refresh()
+            return all_text, title
 
-finally:
-    open('info.txt', 'w').close()
-    file = open("info.txt", "a") #opens a file in the same folder with python script
-    file.write(all_text) #writes all text to a file in the same folder with python script
-    print(all_text)
-    print(src)
-    driver.close()
+    finally:
+        open('info.txt', 'w').close()
+        file = open("info.txt", "a") #opens a file in the same folder with python script
+        file.write(all_text) #writes all text to a file in the same folder with python script
+        print(all_text)
+        print(src)
+        driver.close()
+
+eel.start("index.html", size = (1500, 900))
--- web/script.js
+++ web/script.js
@@ -1,10 +1,11 @@
 
 async function get_text(){
 
-    let all_text = await eel.txt_reader()();
+    query_value = document.getElementById("search").value;
 
-    document.getElementById("main_text").textContent = all_text;
-
+    let all_text = await eel.scraper(query_value)();
+    document.getElementById("main_text").textContent = all_text[0];
+    document.getElementById("result_title").textContent = all_text[1];
 }
 
 $("#submit").click(function(){
