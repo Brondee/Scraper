f7e258e924a527d5f356a70e28a5904ba0dd473d
Artyom <3Westers07P@gmail.com>
2022-02-13 23:29:22 +0300

    trying to add groupping functions, also added multiple query function and passing not article page

--- scraper.py
+++ scraper.py
@@ -16,57 +16,66 @@ def scraper(query, amount):
     all_text = []
     titles = []
     srcs = []
+    queries = query.split()
     counter = 0
     driver = webdriver.Chrome(PATH)
-    driver.get("https://currency.com/search")
-    print(driver.title)
 
-    search = driver.find_element(By.XPATH, "/html/body/div[1]/form[1]/div/label/input")
-    search.send_keys(query)
-    search.send_keys(Keys.RETURN)
+    for x in range(len(queries)):
+
+        driver.get("https://currency.com/search")
+        print(driver.title)
+        search = driver.find_element(By.XPATH, "/html/body/div[1]/form[1]/div/label/input")
+        search.send_keys(queries[x])
+        search.send_keys(Keys.RETURN)
 
 
-    container = WebDriverWait(driver, 10).until(
-        EC.presence_of_element_located((By.CLASS_NAME, "global-search__list"))
-    )
+        container = WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.CLASS_NAME, "global-search__list"))
+        )
 
-    results = container.find_elements(By.CLASS_NAME, "global-search__item")
+        results = container.find_elements(By.CLASS_NAME, "global-search__item")
 
-    try:
-        for i in range(int(amount)):
+        try:
+            for i in range(int(amount)):
             
-            container_el = WebDriverWait(driver, 10).until(
-                EC.presence_of_element_located((By.CLASS_NAME, "global-search__list"))
-            )
+                container_el = WebDriverWait(driver, 10).until(
+                    EC.presence_of_element_located((By.CLASS_NAME, "global-search__list"))
+                )
 
-            results_el = container_el.find_elements(By.CLASS_NAME, "global-search__item")
+                results_el = container_el.find_elements(By.CLASS_NAME, "global-search__item")
 
-            img = results_el[i].find_element(By.TAG_NAME, "img")
-            srcs.append(img.get_attribute("src")) 
+                if results_el[i].find_elements(By.CLASS_NAME, "global-search__img"):
 
-            a = results_el[i].find_element(By.TAG_NAME, "a")
-            a.click()
+                    img = results_el[i].find_element(By.TAG_NAME, "img")
+                    srcs.append(img.get_attribute("src"))
+            
+                else:
+                    continue
+
+                a = results_el[i].find_element(By.TAG_NAME, "a")
+                a.click()
     
         
-            section = WebDriverWait(driver, 5).until(
-                EC.presence_of_element_located((By.CLASS_NAME, "inner-content-article"))
-            )
-            all_text.append(section.text)
-            titles.append(driver.title) 
+                section = WebDriverWait(driver, 5).until(
+                    EC.presence_of_element_located((By.CLASS_NAME, "inner-content-article"))
+                )
 
-            counter += 1
-            driver.back()
-            driver.refresh()
+                all_text.append(section.text)
+                titles.append(driver.title) 
 
-        return titles, srcs, all_text, counter
+                counter += 1
+                driver.back()
+                driver.refresh()
 
-    finally:
-        #open('info.txt', 'w').close()
-        #file = open("info.txt", "a") #opens a file in the same folder with python script
-        #file.write(all_text) #writes all text to a file in the same folder with python script
-        print(all_text)
-        print(srcs)
-        print(counter)
-        print("----------------------------------------------------")
+        finally:
+            #open('info.txt', 'w').close()
+            #file = open("info.txt", "a") #opens a file in the same folder with python script
+            #file.write(all_text) #writes all text to a file in the same folder with python script
+            print(all_text)
+            print(srcs)
+            print(counter)
+            print("----------------------------------------------------")
+
+    return titles, srcs, all_text, counter, queries
 
 eel.start("index.html", size = (1500, 900))
--- web/script.js
+++ web/script.js
@@ -1,7 +1,6 @@
 
 
 
-
 async function get_text(){
 
     query_value = document.getElementById("search").value;
@@ -10,8 +9,22 @@ async function get_text(){
     let python_func = await eel.scraper(query_value, amount_value)();
 
     let counter = python_func[3]
+    let group_counter = 0;
 
     for(let i = 0; i < counter; i++){
+
+        let div_group = document.createElement("div");
+        div_group.className = "group_container";
+
+        let div_group_title = document.createElement("div");
+        div_group_title.className = "group_title_container";
+
+        let p_group_title = document.createElement("p");
+        p_group_title.className = "group_title";
+
+        let img_arrow_down = document.createElement("img");
+        img_arrow_down.className = "arrow_down";
+        img_arrow_down.src = "icons/arrow-down.png";
 
         let div_res = document.createElement('div');
         div_res.className = 'result';
@@ -32,8 +45,22 @@ async function get_text(){
         res_text.className = 'main_text';
 
         let main_div = document.getElementsByClassName("results")[0];
-        main_div.append(div_res);
 
+        if(i == 0) {
+            main_div.append(div_group);
+            div_group.append(div_group_title);
+            div_group_title.append(p_group_title);
+            div_group_title.append(img_arrow_down);
+        }
+        else if(i % amount_value == 0){
+            main_div.append(div_group);
+            div_group.append(div_group_title);
+            div_group_title.append(p_group_title);
+            div_group_title.append(img_arrow_down);
+            group_counter++;
+        }
+
+        div_group.append(div_res);
         div_res.append(div_wrap);
         div_wrap.append(div_title_img);
         div_title_img.append(res_title);
@@ -43,11 +70,13 @@ async function get_text(){
         let func_title = python_func[0][i];
         let func_img = python_func[1][i];
         let func_text = python_func[2][i];
+        let query_return = python_func[4][group_counter];
 
         res_title.innerText = func_title;
         res_img.src = func_img;
         res_img.className = "article_img";
         res_text.innerText = func_text;
+        p_group_title.innerText = query_return;
     }
 }
 
@@ -63,4 +92,9 @@ $("#submit").click(function(){
 
 $("#search").click(function(){
     $(".choice").removeClass("choice_active");
+})
+
+$(".group_title_container").click(function(){
+    $(this).toggleClass("group_title_container_active");
+    $(this).find(".arrow_down").toggleClass("arrow_up");
 })
